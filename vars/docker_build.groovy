9e9078fe177c8087f00bf524a28a7d877d006d7a
Sahil Kr. Mandal <mrsahilmandal@gmail.com>
2025-10-04 13:57:00 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,6 +1,6 @@
 // define function
 def call(String username, String imageName, String imageTag) {
   echo "Building Start...."
-  sh "docker build -t "${username}"/"${imageName}":"${imageTag}" ."
+  sh "docker build -t ${username}/${imageName}:${imageTag} ."
   echo "Building Success!"
 }
